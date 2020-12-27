0884701129655ac8e0b0aa1d0c99dfbb594d5c36
Sally Reynolds <72604111+sallyrreynolds@users.noreply.github.com>
2020-12-27 13:48:25 +0000

    Update forecast

--- src/script.js
+++ src/script.js
@@ -55,10 +55,42 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
+function dispalyForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+      <div class="col-2">
+        <img
+          src="http://openweathermap.org/img/wn/${
+            forecast.weather[0].icon
+          }@2x.png" 
+        />
+        <div class="weather-forecast-temperature">
+          <h5> <strong>
+            ${Math.round(forecast.main.temp_max)}°
+          </strong>
+            ${Math.round(forecast.main.temp_min)}°
+            </h5>
+        </div>
+        <h3>
+          ${formatHours(forecast.dt * 1000)}
+        </h3>
+      </div>
+  `;
+  }
+}
+
 function searchCity(city) {
   let apiKey = "93384e0f4e8b02d826fe829b41b580ce";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(dispalyForecast);
 }
 
 function handleSubmit(event) {
